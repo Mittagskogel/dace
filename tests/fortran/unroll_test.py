e2d49b55d080a7b0541ff35c337f95d7b1c9cc25
Faveo Hörold <faveo.hoerold@cscs.ch>
2025-07-08 11:40:38 +0200

    Create deep copies of loop blocks.

--- tests/fortran/unroll_test.py
+++ tests/fortran/unroll_test.py
@@ -47,19 +47,17 @@ end subroutine main
 SUBROUTINE main(d)
   IMPLICIT NONE
   INTEGER :: idx, d(5)
+  idx = 1
   d(1) = d(1) + 1
+  idx = 2
   d(1) = d(1) + 1
+  idx = 3
   d(1) = d(1) + 1
 END SUBROUTINE main
     """.strip()
     assert got == want
     SourceCodeBuilder().add_file(got).check_with_gfortran()
-    # g = create_singular_sdfg_from_string(sources, 'main')
-    # g.validate()
-    # g.simplify()
-    # d = np.full([5], 5, order="F", dtype=np.int32)
-    # g(d=d)
-    # assert(d[0] == 8)
+
 
 def test_fortran_frontend_loop_unroll_index():
     """Tests whether unrolling transformation correctly replaces indices."""
@@ -74,44 +72,7 @@ end subroutine main
 """).check_with_gfortran().get()
     ast = parse_and_improve(sources)
     ast = unroll_loops(ast)
-    print(ast.children)
-    ast = exploit_locally_constant_variables(ast)
-
-    got = ast.tofortran()
-    want = """
-SUBROUTINE main(d)
-  IMPLICIT NONE
-  INTEGER :: idx, d(5)
-  idx = 1
-  d(1) = d(1) + 1
-  idx = 2
-  d(1) = d(1) + 2
-  idx = 3
-  d(1) = d(1) + 3
-END SUBROUTINE main
-    """.strip()
-    assert got == want
-    SourceCodeBuilder().add_file(got).check_with_gfortran()
-
-
-def test_fortran_frontend_local_assigns():
-    """
-    Tests that local assignment statements are respected by the const replacer.
-    """
-    sources, main = SourceCodeBuilder().add_file("""
-subroutine main(d)
-  implicit none
-  integer :: idx, d(5)
-  idx = 1
-  d(1) = d(1) + idx
-  idx = 2
-  d(1) = d(1) + idx
-  idx = 3
-  d(1) = d(1) + idx
-end subroutine main
-""", 'main').check_with_gfortran().get()
-    ast = parse_and_improve(sources)
-    print(ast.children)
+    # print(ast.children)
     ast = exploit_locally_constant_variables(ast)
 
     got = ast.tofortran()
@@ -133,3 +94,4 @@ END SUBROUTINE main
 
 if __name__ == "__main__":
     test_fortran_frontend_loop_unroll()
+    test_fortran_frontend_loop_unroll_index()
